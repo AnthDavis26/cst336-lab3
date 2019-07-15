d8aafbbe2401be0ac141562557b0128b7135934d
AnthDavis26 <42490176+AnthDavis26@users.noreply.github.com>
2019-07-14 18:33:31 -0700

    push

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
 	<head>
 		<title> Sign Up Page </title>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 	</head>
 	<body>
 	
@@ -14,8 +15,8 @@
 			
 			Zip Code: 	<input type="text" name="zip" id="zip"><br />
 			City:		<span id="city"></span><br />
-			Latitude: 	<br />
-			Longitude: 	<br /><br />
+			Latitude: 	<span id="latitude"></span><br />
+			Longitude: 	<span id="longitude"></span><br /><br />
 			State:
 			<select id="state" name="state">
 				<option value="">Select One</option>
@@ -32,5 +33,22 @@
 			
 			<input type="submit" value="Sign up!">
 			
+			<script>
+				// Displaying City from API after typing a zip code
+				$("#zip").on("change",function(){
+					//alert($("#zip").val());
+				
+					$.ajax({
+						method: "GET",
+						url: "https://cst336.herokuapp.com/projects/api/cityInfoAPI.php",
+						dataType: "json",
+						data: { "zip" : $("#zip").val() },
+
+						success: function(result,status) {
+							alert(result);
+						}
+					}); //ajax
+					
+			</script>
 	</body>
 </html>
--- push.bat
+++ push.bat
@@ -1,3 +1,3 @@
 git add .
-git commit -m "First push"
+git commit -m "push"
 git push
