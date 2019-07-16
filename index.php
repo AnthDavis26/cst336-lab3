53d7335331027e29e1435ebdb50bff55eebdbacb
AnthDavis26 <42490176+AnthDavis26@users.noreply.github.com>
2019-07-16 00:12:11 -0700

    push

--- index.php
+++ index.php
@@ -2,12 +2,13 @@
 <html>
 	<head>
 		<title> Sign Up Page </title>
+		<link  href="css/styles.css" rel="stylesheet" type="text/css" />
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 	</head>
 	<body>
 	
 		<h1> Sign Up </h1>
-		
+		<form id="signupForm" method="post" action="welcome.php">
 			First Name:	<input type="text" name="fName"><br />
 			Last Name:	<input type="text" name="lName"><br />
 			Gender: 	<input type="radio" name="gender" value="m"> Male
@@ -20,35 +21,149 @@
 			State:
 			<select id="state" name="state">
 				<option value="">Select One</option>
-				<option value="ca">California</option>
-				<option value="ny">New York</option>
-				<option value="tx">Texas</option>
 			</select><br />
 			
 			Select a County: <select id="county"></select><br /><br />
 			
 			Desired Username:<input type="text"		id="username"	name="username"></br />
+							 <span id="usernameError"></span><br />
 			Password: 		 <input type="password"	id="password"	name="password"></br />
 			Password Again:  <input type="password"	id="passwordAgain"></br />
+							 <span id="passwordAgainError"></span><br /><br />
 			
 			<input type="submit" value="Sign up!">
+		</form>
+		
+		<script>
 			
-			<script>
-				// Displaying City from API after typing a zip code
-				$("#zip").on("change",function(){
-					//alert($("#zip").val());
-				
-					$.ajax({
-						method: "GET",
-						url: "https://cst336.herokuapp.com/projects/api/cityInfoAPI.php",
-						dataType: "json",
-						data: { "zip" : $("#zip").val() },
-
-						success: function(result,status) {
-							alert(result);
-						}
-					}); //ajax
+			// Variables
+			var usernameAvailable = false;
+			
+			
+			// Populate states list upon page load
+			window.onload = function () {
+				$.ajax ({
+					method: "GET",
+					url: "https://cst336.herokuapp.com/projects/api/state_abbrAPI.php",
+					dataType: "json",
 					
-			</script>
+					success: function(result, status) {
+						
+						for (let i=0; i < result.length; i++){
+							$("#state").append("<option value='" + result[i].usps + "'>" + result[i].state + "</option>");
+						}
+					}
+				});
+			};
+			
+		
+			// Displaying City from API after typing a zip code
+			$("#zip").on("change",function(){
+				//alert($("#zip").val());
+				
+				$.ajax({
+					method: "GET",
+					url: "https://cst336.herokuapp.com/projects/api/cityInfoAPI.php",
+					dataType: "json",
+					data: { "zip" : $("#zip").val() },
+
+					success: function(result,status) {
+						//alert(result.city);
+						$("#city").html(result.city);
+						$("#latitude").html(result.latitude);
+						$("#longitude").html(result.longitude);
+					}
+				}); // ajax
+			}); // zip
+			
+			
+			// STATE
+			$("#state").on("change",function(){
+				//alert($("#state").val());
+				
+				// Clear county list upon state change
+				$("#county").empty();
+				
+				$.ajax({
+					method: "GET",
+					url: "https://cst336.herokuapp.com/projects/api/countyListAPI.php",
+					dataType: "json",
+					data: { "state" : $("#state").val() },
+
+					success: function(result,status) {
+						//alert(result[0].county);
+						
+						for (let i=0; i < result.length; i++){
+							$("#county").append("<option>" + result[i].county + "</option>");
+						}
+					}
+				}); // ajax
+			}); // state
+			
+			
+			// USERNAME
+			$("#username").change(function() {
+				//alert($("#username").val());
+				
+				$.ajax({
+					method: "GET",
+					url: "https://cst336.herokuapp.com/projects/api/usernamesAPI.php",
+					dataType: "json",
+					data: { "username" : $("#username").val() },
+					success: function(result, status) {
+						
+						if (result.available){
+							usernameAvailable = true;
+							$("#usernameError").html("Username is available!");
+							$("#usernameError").css("color", "green");
+						}
+						else {
+							usernameAvailable = false;
+							$("#usernameError").html("Username unavailable");
+							$("#usernameError").css("color", "red");
+						}
+					}
+					
+				}); // ajax
+			}); // username
+			
+			
+			// Check the form for errors upon submission
+			$("#signupForm").on("submit", function() {
+				//alert("Submitting form...");
+				
+				if (!isFormValid()){
+					event.preventDefault();
+				}
+			});
+			
+			function isFormValid() {
+				var isValid = true;
+				
+				if (!usernameAvailable){
+					isValid = false;
+				}
+				
+				if ($("#username").val().length == 0) {
+					$("#usernameError").html("Username is required");
+					isValid = false;
+				}
+				
+				if ($("#password").val() != $("#passwordAgain").val()){
+					$("#passwordAgainError").html("Passwords do not match.");
+					isValid = false;
+				}
+				
+				if ($("#password").val().length < 6) {
+					$("#passwordAgainError").html("Your password must contain at least 6 characters.");
+					isValid = false;
+				}
+				
+				return isValid;
+			}
+			
+			
+			
+		</script>
 	</body>
 </html>
---
+++ welcome.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> Sign Up Page </title>
+		<link  href="css/styles.css" rel="stylesheet" type="text/css" />
+	</head>
+	<body>
+	
+		<div id="congrats"><h1>Congratulations!</h1><br/><br/>However, this is a lab assignment, so you haven't created a real account.</div>
+	
+	</body>
+</html>
